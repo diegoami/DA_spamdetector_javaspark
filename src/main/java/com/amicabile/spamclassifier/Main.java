17aa7ce7ee29a6dbc820d6044acad04c55b4b6a4
Diego Amicabile <diego.amicabile@gmail.com>
2019-07-09 17:54:49 +0200

    Executing spam messenger prediction

--- src/main/java/com/amicabile/spamclassifier/Main.java
+++ src/main/java/com/amicabile/spamclassifier/Main.java
@@ -5,48 +5,66 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.ml.Pipeline;
+import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.ml.PipelineStage;
+import org.apache.spark.ml.classification.LogisticRegression;
+import org.apache.spark.ml.feature.Tokenizer;
+import org.apache.spark.ml.feature.HashingTF;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
 
-import java.util.function.Function;
+
+import org.apache.spark.ml.feature.RegexTokenizer;
+import org.apache.spark.ml.feature.Tokenizer;
+import sun.misc.JavaLangAccess;
+
 
 public class Main {
 
-
-    static private String stripQuotes(String string, char quoteChar){
-
-        if(string.length() > 1 && ((string.charAt(0) == quoteChar) && (string.charAt(string.length() - 1) == quoteChar))){
-            return string.substring(1, string.length() - 1);
-        }
-
-        return string;
-    }
-
-
     public static void main(String[] args) {
 
-        SparkConf conf = new SparkConf()
-                .setAppName("Word Count").setMaster("local");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> data = sc.textFile("spam_out.csv");
-        //JavaSQLContext sqlContext = new JavaSQLContext(sc); // For previous versions
-        SQLContext sqlContext = new SQLContext(sc); // In Spark 1.3 the Java API and Scala API have been unified
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Java Spark SQL Example")
+                .config("spark.master", "local")
+                .getOrCreate();
+
+        StructType schema = new StructType()
+                .add("message", "string")
+                .add("label", "int");
 
 
-        JavaRDD<Record> rdd_records = data.map(
-                line -> {
-                    try {
-                        String message = stripQuotes(line.substring(0, line.length() - 2), '"');
+        Dataset<Row> df = spark.read()
+                .option("mode", "DROPMALFORMED")
+                .schema(schema)
+                .csv("spam_out.csv");
+                //.map(line -> new JavaLabeledDocument(0L, line[0], Double.parseDouble(line[1])));
 
-                        String labelString = line.substring(line.length() - 1, line.length());
-                        int label = Integer.parseInt(labelString );
-                        System.out.format("Processing row %s\n", message);
-                        return new Record(message, label);
-                    } catch (NumberFormatException nfe) {
-                        System.out.format("Skipping row  %s\n", nfe.getMessage());
-                        return new Record("", 0);
-                    }
-                }
-        );
+        df.show();
+
+        Tokenizer tokenizer = new Tokenizer()
+                .setInputCol("message")
+                .setOutputCol("words");
+        HashingTF hashingTF = new HashingTF()
+                .setNumFeatures(1000)
+                .setInputCol(tokenizer.getOutputCol())
+                .setOutputCol("features");
+        LogisticRegression lr = new LogisticRegression()
+                .setMaxIter(10)
+                .setRegParam(0.001);
+        Pipeline pipeline = new Pipeline()
+                .setStages(new PipelineStage[] {tokenizer, hashingTF, lr});
+        PipelineModel model = pipeline.fit(df);
+
+        Dataset<Row> predictions = model.transform(df);
+        for (Row r : predictions.select( "message", "probability", "prediction").collectAsList()) {
+            System.out.println("(" + r.get(0)  + ") --> prob=" + r.get(1)
+                    + ", prediction=" + r.get(2));
+        }
 
 
     }
