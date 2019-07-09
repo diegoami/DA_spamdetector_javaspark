1e76dd4c2dd86611c00baac3d36c0301669aefd7
Diego Amicabile <diego.amicabile@gmail.com>
2019-07-09 23:06:31 +0200

    All model files for prediction

--- src/main/java/com/amicabile/spamclassifier/Main.java
+++ src/main/java/com/amicabile/spamclassifier/Main.java
@@ -1,33 +1,30 @@
 package com.amicabile.spamclassifier;
 
-import org.apache.commons.io.LineIterator;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
+
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.ml.Pipeline;
 import org.apache.spark.ml.PipelineModel;
 import org.apache.spark.ml.PipelineStage;
 import org.apache.spark.ml.classification.LogisticRegression;
-import org.apache.spark.ml.evaluation.RegressionEvaluator;
 import org.apache.spark.ml.feature.Tokenizer;
 import org.apache.spark.ml.feature.HashingTF;
-import org.apache.spark.ml.feature.CountVectorizer;
 
-import org.apache.spark.ml.tuning.TrainValidationSplit;
-import org.apache.spark.ml.tuning.TrainValidationSplitModel;
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics;
 import org.apache.spark.mllib.evaluation.MulticlassMetrics;
 import org.apache.spark.mllib.linalg.Matrix;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
-import static org.apache.spark.sql.functions.col;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.apache.spark.sql.functions.col;
 
 
 public class Main {
@@ -40,6 +37,9 @@ public class Main {
                 .config("spark.master", "local")
                 .getOrCreate();
 
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+        jsc.setLogLevel("WARN");
+
         StructType schema = new StructType()
                 .add("message", "string")
                 .add("label", "int");
@@ -49,9 +49,8 @@ public class Main {
                 .option("mode", "DROPMALFORMED")
                 .schema(schema)
                 .csv("spam_out.csv");
-                //.map(line -> new JavaLabeledDocument(0L, line[0], Double.parseDouble(line[1])));
 
-        Dataset<Row>[] splits = df.randomSplit(new double[] {0.8, 0.2}, 12345);
+        Dataset<Row>[] splits = df.randomSplit(new double[] {0.8, 0.2}, 42);
         Dataset<Row> trainingDf = splits[0];
         Dataset<Row> testDf = splits[1];
 
@@ -59,8 +58,7 @@ public class Main {
         Tokenizer tokenizer = new Tokenizer()
                 .setInputCol("message")
                 .setOutputCol("words");
-        CountVectorizer countVectorizer = new CountVectorizer().setInputCol(tokenizer.getOutputCol())
-                .setOutputCol("features");
+        //CountVectorizer countVectorizer = new CountVectorizer().setInputCol(tokenizer.getOutputCol()).setOutputCol("features");
         HashingTF hashingTF = new HashingTF()
                 .setNumFeatures(3000)
                .setInputCol(tokenizer.getOutputCol())
@@ -74,6 +72,7 @@ public class Main {
         Dataset<Row> testPredictions = model.transform(testDf).withColumn("label", testDf.col("label").cast(DataTypes.DoubleType));
 
         Dataset<Row> predictions = model.transform(df).withColumn("label", df.col("label").cast(DataTypes.DoubleType));
+
         //for (Row r : predictions.select( "message", "probability", "prediction").collectAsList()) {
         //    System.out.println("(" + r.get(0)  + ") --> prob=" + r.get(1)
         //            + ", prediction=" + r.get(2));
@@ -94,7 +93,30 @@ public class Main {
 // Confusion matrix
         Matrix confusion = metrics.confusionMatrix();
         System.out.println("Confusion matrix: \n" + confusion);
+        try {
+            pipeline.write().overwrite().save("pipeline");
+            model.write().overwrite().save("model");
+        } catch (java.io.IOException ioe) {
+            System.out.format("Cannot save model to pipeline.model: %s\n", ioe.getMessage());
+        }
 
+        PipelineModel loadedModel = PipelineModel.load("model");
+        List<Row> rowList = Arrays.asList(
+                RowFactory.create("Winner! You have won a car"),
+                RowFactory.create("I feel bad today"),
+                RowFactory.create("Please call our customer service representative"),
+                RowFactory.create("Your free ringtone is waiting to be collected. Simply text the password")
+        );
 
+        StructType schemaPred = new StructType()
+                .add("message", "string");
+
+        Dataset<Row> rowDf = spark.createDataFrame(rowList, schemaPred);
+        Dataset<Row> predictionsLoaded = loadedModel.transform(rowDf);
+        System.out.println(predictionsLoaded);
+        for (Row r : predictionsLoaded.select( "message", "probability", "prediction").collectAsList()) {
+            System.out.println("(" + r.get(0)  + ") --> prob=" + r.get(1)
+                    + ", prediction=" + r.get(2));
+        }
     }
 }
