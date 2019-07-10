c7ea26c590601bdbca9cc2f4a6cca26295ec128a
Diego Amicabile <diego.amicabile@gmail.com>
2019-07-10 15:39:34 +0200

    Correct docker file in README

--- src/main/java/com/amicabile/spamclassifier/TestModel.java
+++ src/main/java/com/amicabile/spamclassifier/TestModel.java
@@ -2,20 +2,12 @@ package com.amicabile.spamclassifier;
 
 
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.ml.Pipeline;
 import org.apache.spark.ml.PipelineModel;
-import org.apache.spark.ml.PipelineStage;
-import org.apache.spark.ml.classification.LogisticRegression;
-import org.apache.spark.ml.feature.HashingTF;
-import org.apache.spark.ml.feature.Tokenizer;
-import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics;
-import org.apache.spark.mllib.evaluation.MulticlassMetrics;
-import org.apache.spark.mllib.linalg.Matrix;
+
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
 
 import java.util.Arrays;
